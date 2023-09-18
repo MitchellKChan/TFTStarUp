5e1111015c751cf8025bd8d71ee47e58ff2968d4
Mitchell Chan <mitchellkchan@gmail.com>
2023-09-17 23:08:03 -0700

    implement champion image rendering; implement refresh and exp button event handling

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import Champions from "./scripts/champions";
 import Shop from "./scripts/shop";
 
-// Declare champions object from champions module via fetchChampions
-const champions = new Champions();
-
-// Declare shop object for tracking level, gold, tier odds, and shop units
-const shop = new Shop();
+// Initialize champions variable to be assigned to a Champions object once champion info is fetched
+let champions;
+try {
+  const res = await fetch("https://ddragon.leagueoflegends.com/cdn/13.18.1/data/en_US/tft-champion.json");
+  if (res.ok) {
+      const promise = await res.json();
+      const list = promise.data;
+      const keys = Object.keys(list);
+      champions = new Champions(list, keys);
+  } else {
+      throw res;
+  }
+} catch(err) {
+  console.log(err);
+}
 
 // Declare object for the page's body
 const body = document.querySelector("body");
+
+// Declare shop object for tracking level, gold, tier odds, and shop units
+const shop = new Shop(3, 2, 4, champions);
 
 // Declare objects for the top section, which contains the title, timer,
 // nav links, and settings
@@ -25,13 +38,21 @@ const bench = document.createElement("div");
 const shopEl = document.createElement("div");
 const bottomSection = document.createElement("div");
 
-// Declare object button for testing AJAX requests to Riot Games'
-// Data Dragon CDN under the following URLs:
-// - Champion JSON: https://ddragon.leagueoflegends.com/cdn/13.18.1/data/en_US/tft-champion.json
-// - Champion Image: https://ddragon.leagueoflegends.com/cdn/13.18.1/img/tft-champion/<champion_image_name>
-const dataDragonButton = document.createElement("button");
-dataDragonButton.innerText = "Data Dragon Request";
-dataDragonButton.classList.add("data-dragon");
+// Declare objects contained by the shop object, which are the 
+// current level, current shop odds, current gold, progress to 
+// next level, buy exp button, refresh button, and shop units
+const level = document.createElement("div");
+const odds = document.createElement("div");
+const levelProgress = document.createElement("div");
+const gold = document.createElement("div");;
+const expButton = document.createElement("button");
+const refreshButton = document.createElement("button");
+const levelInfo = document.createElement("div");
+const shopButtons = document.createElement("div");
+const shopUnits = document.createElement("div");
+
+// const unitImageTester = document.createElement("img");
+// unitImageTester.src = champions.championImage(champions.randomChampion());
 
 // set inner text and classess for top section objects
 title.innerText = "TFT Star Up";
@@ -51,22 +72,87 @@ topRight.classList.add("top-right", "section",);
 topSection.classList.add("top");
 topSection.append(title, timer, topRight);
 
-// set inner text and classess for top section objects
+// set inner text and classess for bottom section objects
 bench.innerText = "bench";
 bench.classList.add("bench", "section");
 
-shopEl.innerText = "shop";
 shopEl.classList.add("shop", "section");
 
 bottomSection.classList.add("bottom");
 bottomSection.append(bench, shopEl);
 
-// add event listener to dataDragonButton to test AJAX request
-dataDragonButton.addEventListener("click", e => {
-  e.preventDefault();
-  // console.log(champions.list);
-  // console.log(shop);
-  // console.log(Shop.tierOdds);
+// set inner text and clases for shop objects
+level.innerText = shop.level;
+level.classList.add("section");
+
+odds.innerText = `${Shop.tierOdds[shop.level]}`;
+odds.classList.add("section");
+
+levelProgress.innerText = `${shop.currentEpx}/${shop.expToNextLevel}`;
+levelProgress.classList.add("section");
+
+levelInfo.classList.add("section", "shop-level-info")
+levelInfo.append(level, odds, levelProgress);
+
+gold.innerText = "gold";
+gold.classList.add("section");
+
+expButton.innerText = "exp button";
+expButton.classList.add("section", "exp-button");
+
+refreshButton.innerText = "refresh button";
+refreshButton.classList.add("section", "refresh-button");
+
+shopButtons.classList.add("shop-buttons");
+shopButtons.append(expButton, refreshButton);
+
+shopUnits.classList.add("section", "shop-units");
+shop.refresh();
+Object.values(shop.units).forEach(slot => {
+  const slotEl = document.createElement("div");
+  const icon = document.createElement("img");
+  icon.src = shop.champions.championImage(slot);
+  slotEl.append(icon);
+  slotEl.classList.add("section", "shop-unit");
+  shopUnits.append(slotEl);
 });
 
-body.append(topSection,dataDragonButton, bottomSection);
+shopEl.append(levelInfo, gold, shopButtons, shopUnits);
+
+// function to handle events that trigger shop refreshes
+function handleRefresh(event) {
+  event.preventDefault();
+  shop.refresh();
+  Object.values(shop.units).forEach((slot, i) => {
+    shopUnits.children[i].children[0].src = shop.champions.championImage(slot);
+  });
+}
+
+// function to handle events that trigger buying experience
+function handleBuyExp(event) {
+  event.preventDefault();
+  if (shop.level < 9) {
+    shop.buyExp();
+    levelProgress.innerText = `${shop.currentEpx}/${shop.expToNextLevel}`;
+    level.innerText = shop.level;
+  }
+}
+
+// add event listener to refreshButton to test shop refreshing
+refreshButton.addEventListener("click", e => {
+  handleRefresh(e);
+});
+
+body.addEventListener("keydown", e => {
+  if (e.code === "KeyD") handleRefresh(e);
+});
+
+expButton.addEventListener("click", e => {
+  handleBuyExp(e);
+})
+
+body.addEventListener("keydown", e => {
+  if (e.code === "KeyF") handleBuyExp(e);
+});
+
+body.append(topSection, bottomSection);
--- src/scripts/shop.js
+++ src/scripts/shop.js
@@ -1,13 +1,36 @@
 class Shop {
-    constructor(startingLevel = 3, currentEpx = 2, expToNextLevel = 4) {
+    constructor(startingLevel = 3, currentEpx = 2, expToNextLevel = 4, champions) {
         this.level = startingLevel;
         this.currentEpx = currentEpx;
         this.expToNextLevel = expToNextLevel;
-        this.slot1;
-        this.slot2;
-        this.slot3;
-        this.slot4;
-        this.slot5;
+        this.champions = champions;
+        this.units = {
+            slot1: "slot 1",
+            slot2: "slot 2",
+            slot3: "slot 3",
+            slot4: "slot 4",
+            slot5: "slot 5"
+        };
+    }
+
+    refresh() {
+        const champions = this.champions;
+        Object.keys(this.units).map(key => {
+            this.units[key] = champions.randomChampion();
+        });
+    }
+
+    buyExp() {
+        this.currentEpx += 4;
+        this.levelUp();
+    }
+
+    levelUp() {
+        if (this.currentEpx >= this.expToNextLevel && this.level < 9) {
+            this.level++;
+            this.currentEpx = this.currentEpx - this.expToNextLevel;
+            this.expToNextLevel = Shop.levelExp[this.level];
+        }
     }
 }
 
@@ -22,5 +45,15 @@ Shop.tierOdds = {
     8: [0.16, 0.20, 0.35, 0.25, 0.04],
     9: [0.09, 0.15, 0.30, 0.30, 0.16],
 }
+
+Shop.levelExp = {
+    2: 2,
+    3: 6,
+    4: 10,
+    5: 24,
+    6: 40,
+    7: 60,
+    8: 84,
+};
 
 export default Shop;
